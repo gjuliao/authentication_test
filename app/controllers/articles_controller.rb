1f5a168097334e3d81405a690b8d06d52f9feebc
gjuliao <gjuliao32@gmail.com>
2024-06-17 12:32:48 -0500

    Added create method in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,14 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+
+    def create
+        @article.create(article_params)
+        if @article.save
+            notice[:success] = 'Article created succesfully'
+        else
+            notice[:alert] = 'Article not saved'
+        end
+    end
     
 end
