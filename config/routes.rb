e79f4d898f22758e4c20d11972abed13b4f54c1b
gjuliao <gjuliao32@gmail.com>
2024-06-18 12:18:34 -0500

    Added user routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
   resources :articles
+  resources :user, except: %w[:new]
+  get '/signup', to: 'users#new'
 end
