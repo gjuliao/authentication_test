4fb5398554a69144e9879152f8df188b445b4b7b
gjuliao <gjuliao32@gmail.com>
2024-06-19 14:12:14 -0500

    Added popup navbar with stimulus

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+    static targets = [ "menu", "hamburguer", "cross" ];
+
+    static values = { isOpen: { type: Boolean, default: false } };
+
+  connect() {
+    console.log('hello world')
+  }
+
+  menu() {
+    console.log('you clicked me')
+    console.log(this.menuTarget)
+
+    this.isOpenValue ? this.hide() : this.show();
+
+    this.isOpenValue = !this.isOpenValue;
+  }
+
+  show(){
+    this.menuTarget.style.display = "block";
+    this.hamburguerTarget.style.display = "block";
+    this.crossTarget.style.display = "none";
+  }
+
+  hide(){
+    this.menuTarget.style.display = "none";
+    this.hamburguerTarget.style.display = "none";
+    this.crossTarget.style.display = "block";
+  }
+}
