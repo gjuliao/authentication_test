ea4df544c880e6405f621485df36d6b60fdf4358
gjuliao <gjuliao32@gmail.com>
2024-06-24 16:18:15 -0500

    Updated behaviour for rendering articles at index with turbo stream

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
     before_action :authorized, except: [:index]
 
     def index
-        @articles = Article.all
+        @articles = Article.order(created_at: :desc)
     end
 
     def new
