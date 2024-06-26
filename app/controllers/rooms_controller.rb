594f7288bf4fc55ffc90905996d0ff3f24cfa025
gjuliao <gjuliao32@gmail.com>
2024-06-26 12:49:55 -0500

    hotwire not working

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,29 @@
 class RoomsController < ApplicationController
 
     def index
+        @rooms = Room.order(created_at: :desc)
+        @room = Room.new
+    end
+
+    def create
+        @room = Room.new(room_params)
+        if @room.save
+            respond_to do |format|
+                
+                format.html {
+                    flash[:notice] = 'Room saved succesfully'
+                    redirect_to rooms_path
+                }
+            end
+        else
+            flash[:alert] = "Room not saved"
+            render "root#index"
+        end
+    end
+
+    private
+
+    def room_params
+        params.require(:room).permit(:name)
     end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
     validates :name, presence: true
     has_many :messages
+
+    broadcasts_to -> (room){ :rooms_list }
 end
