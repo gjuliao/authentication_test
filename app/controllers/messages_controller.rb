5b848bcf71b941e304855f0aeb8bdc413e71589c
gjuliao <gjuliao32@gmail.com>
2024-06-27 14:23:50 -0500

    Added methods to messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,43 @@
+class MessagesController < ApplicationController
+
+    before_action :set_room
+
+    def index
+      
+    end
+
+    def create
+        @message = Message.new(message_params)
+        if @message.save
+            respond_to do |format|
+                format.html {
+                    flash[:notice] = 'Message created succesfully'
+                    redirect_to @message.room
+                }
+            end
+        else 
+            respond_to do |format|
+                format.html {
+                    flash.now[:alert] = 'Message NOT created'
+                    render @message.room
+                }
+            end
+        end
+    end
+
+    private
+
+    def set_message
+        @message = Message.find(params[:id])
+    end
+
+    def set_room
+        @room = Room.find(params[:id])
+    end
+
+    def message_params
+        params.require(:message).permit(:content)
+    end
+
+
+end
