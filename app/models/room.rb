9800f87293c377c250bfc68a466fc7c44722ae1d
gjuliao <gjuliao32@gmail.com>
2024-06-27 13:49:50 -0500

    Fixed realtime broadcasting on rooms_list

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
+    broadcasts_to -> (room){ :rooms_list }, inserts_by: :prepend
     validates :name, presence: true
     has_many :messages
 
-    broadcasts_to -> (room){ :rooms_list }
+    # after_commit -> { broadcast_prepend_to :rooms_list, target: "rooms_list", partial: "rooms/room", locals: { room: self }, inserts_by: :prepend }, on: :create
 end
